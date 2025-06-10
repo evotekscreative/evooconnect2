cb32b5d61e4c42565af20bf927434297cffc2fca
Windha-Kusuma-Dewi <windhaksm@gmail.com>
2025-06-10 14:34:43 +0700

    company-dashboard->sidebar2

--- frontend/src/components/CompanyDashboard/Sidebar/Sidebar.jsx
+++ frontend/src/components/CompanyDashboard/Sidebar/Sidebar.jsx
@@ -35,29 +35,6 @@ export default function Sidebar() {
                             collapseShow
                         }
                     >
-                        {/* Collapse header */}
-                        <div className="md:min-w-full md:hidden block pb-4 mb-4 border-b border-solid border-gray-200">
-                            <div className="flex flex-wrap">
-                                <div className="w-6/12">
-                                    <Link
-                                        className="md:block text-left md:pb-2 text-gray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
-                                        to="/"
-                                    >
-                                        Notus React
-                                    </Link>
-                                </div>
-                                <div className="w-6/12 flex justify-end">
-                                    <button
-                                        type="button"
-                                        className="cursor-pointer text-black opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent"
-                                        onClick={() => setCollapseShow("hidden")}
-                                    >
-                                        <i className="fas fa-times"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-
                         {/* Form */}
                         <form className="mt-6 mb-4 md:hidden">
                             <div className="mb-3 pt-0">
@@ -87,7 +64,7 @@ export default function Sidebar() {
                                             ? "text-sky-500 hover:text-sky-600"
                                             : "text-gray-700 hover:text-gray-500")
                                     }
-                                    to="/admin/dashboard"
+                                    to="#"
                                 >
                                     <i
                                         className={
@@ -109,12 +86,12 @@ export default function Sidebar() {
                                             ? "text-sky-500 hover:text-sky-600"
                                             : "text-gray-700 hover:text-gray-500")
                                     }
-                                    to="/admin/settings"
+                                    to="#"
                                 >
                                     <i
                                         className={
                                             "fas fa-tools mr-2 text-sm " +
-                                            (window.location.href.indexOf("/admin/settings") !== -1
+                                            (window.location.href.indexOf("#") !== -1
                                                 ? "opacity-75"
                                                 : "text-gray-300")
                                         }
@@ -127,16 +104,16 @@ export default function Sidebar() {
                                 <Link
                                     className={
                                         "text-xs uppercase py-3 font-bold block " +
-                                        (window.location.href.indexOf("/admin/tables") !== -1
+                                        (window.location.href.indexOf("#") !== -1
                                             ? "text-sky-500 hover:text-sky-600"
                                             : "text-gray-700 hover:text-gray-500")
                                     }
-                                    to="/admin/tables"
+                                    to="#"
                                 >
                                     <i
                                         className={
                                             "fas fa-table mr-2 text-sm " +
-                                            (window.location.href.indexOf("/admin/tables") !== -1
+                                            (window.location.href.indexOf("#") !== -1
                                                 ? "opacity-75"
                                                 : "text-gray-300")
                                         }
@@ -149,16 +126,16 @@ export default function Sidebar() {
                                 <Link
                                     className={
                                         "text-xs uppercase py-3 font-bold block " +
-                                        (window.location.href.indexOf("/admin/report") !== -1
+                                        (window.location.href.indexOf("#") !== -1
                                             ? "text-sky-500 hover:text-sky-600"
                                             : "text-gray-700 hover:text-gray-500")
                                     }
-                                    to="/admin/report"
+                                    to="#"
                                 >
                                     <i
                                         className={
                                             "fas fa-table mr-2 text-sm " +
-                                            (window.location.href.indexOf("/admin/report") !== -1
+                                            (window.location.href.indexOf("#") !== -1
                                                 ? "opacity-75"
                                                 : "text-gray-300")
                                         }
@@ -171,16 +148,16 @@ export default function Sidebar() {
                                 <Link
                                     className={
                                         "text-xs uppercase py-3 font-bold block " +
-                                        (window.location.href.indexOf("/admin/list-company") !== -1
+                                        (window.location.href.indexOf("#") !== -1
                                             ? "text-sky-500 hover:text-sky-600"
                                             : "text-gray-700 hover:text-gray-500")
                                     }
-                                    to="/admin/list-company"
+                                    to="#"
                                 >
                                     <i
                                         className={
                                             "fas fa-table mr-2 text-sm " +
-                                            (window.location.href.indexOf("/admin/list-company") !== -1
+                                            (window.location.href.indexOf("#") !== -1
                                                 ? "opacity-75"
                                                 : "text-gray-300")
                                         }
